28d5c2c2596339c76a632a817beae389e4495cda
amc01 <amc001@ucsd.edu>
2021-11-06 16:59:41 -0700

    uncomment part2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -59,17 +59,17 @@ function initializeServiceWorker() {
    *  Initialize the service worker set up in sw.js
    */
 
-  //  if ('serviceWorker' in navigator) {
-  //   window.addEventListener('load', function() {
-  //     navigator.serviceWorker.register('sw.js').then(function(registration) {
-  //       // Registration was successful
-  //       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  //     }, function(err) {
-  //       // registration failed :(
-  //       console.log('ServiceWorker registration failed: ', err);
-  //     });
-  //   });
-  // }
+   if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
 }
 
